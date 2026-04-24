e13fed4b06e1b50ad421e14fda940ae56df6a8f0
ashraful-hoda <asrafulhoda789@gmail.com>
2026-04-24 12:50:43 +0600

    understand heroUI and use switch theme

--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -1,3 +1,4 @@
+import AddTask from "@/components/AddTask";
 import TasksCard from "@/components/TasksCard";
 import { getTasks } from "@/lib/tasks";
 
@@ -7,7 +8,8 @@ const TasksPage = async() => {
     return (
         <div className="container mx-auto">
             <h2>Tasks: {tasks.length}</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10"> 
+            <AddTask></AddTask>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-10"> 
                 {
                     tasks.map(task => <TasksCard key={task.id} task={task}></TasksCard>)
                 }
